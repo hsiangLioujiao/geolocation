52c381460157f76558ca43d9075f9e4d6f1c2ba3
hsiangLioujiao <99463623+hsiangLioujiao@users.noreply.github.com>
2024-07-16 17:08:36 +0800

    v0.003
    road test

--- geolocation.py
+++ geolocation.py
@@ -5,7 +5,7 @@ Created on Mon Jul 15 09:28:41 2024
 @author: g_s_s
 """
 
-
+# https://discuss.streamlit.io/t/web-geolocation-api-to-get-users-location/9493/7
 # https://pypi.org/project/streamlit-js-eval/
 # https://github.com/aghasemi/streamlit_js_eval/tree/master
 
@@ -17,66 +17,140 @@ from streamlit_js_eval import streamlit_js_eval, get_geolocation
 import pandas as pd
 import matplotlib.pyplot as plt
 import time
+import threading
+import queue
 
 plt.rcParams["font.sans-serif"] = "Microsoft JhengHei"
 plt.rcParams["axes.unicode_minus"] = False
 
 
-# st.write(f"User agent is _{streamlit_js_eval(js_expressions='window.navigator.userAgent', want_output = True, key = 'UA')}_")
-# st.write(f"Screen width is _{streamlit_js_eval(js_expressions='screen.width', want_output = True, key = 'SCR')}_")
-# st.write(f"Browser language is _{streamlit_js_eval(js_expressions='window.navigator.language', want_output = True, key = 'LANG')}_")
-st.write(f"網頁: { streamlit_js_eval(js_expressions='window.location.origin', want_output = True, key = 'LOC')}_")
+# # https://discuss.streamlit.io/t/realtime-data-update-to-streamlit-from-python-variable/35890/2
+# q = queue.Queue()
 
-# (待修正)
-# st.write(f"my coordinates is _{ streamlit_js_eval(js_expressions='window.getCurrentPosition', want_output = True, key = 'myLOC')}_")
-
-
-# Copying to clipboard only works with a HTTP connection
-# copy_to_clipboard("Text to be copied!", "Copy something to clipboard (only on HTTPS)", "Successfully copied" , component_key = "CLPBRD")
-
-# Share something using the sharing API
-# create_share_link(dict({'title': 'streamlit-js-eval', 'url': 'https://github.com/aghasemi/streamlit_js_eval', 'text': "A description"}), "Share a URL (only on mobile devices)", 'Successfully shared', component_key = 'shdemo')
+# def test_run():
+#     for x in range(1, 100):
+#         time.sleep(1)
+#         val = x
+#         multiply = val * 10        
+#         q.put((val, multiply))
 
 
-# if st.checkbox("Check my location"):
-#     loc = get_geolocation()
-#     st.write(f"Your coordinates are {loc}")
+# is_exit_target_if_main_exits = True
+# threading.Thread(
+#     target=test_run,
+#     daemon=is_exit_target_if_main_exits).start()
 
+# # dashboard title
+# st.title("Streamlit Learning")
+
+# # creating a single-element container.
+# placeholder = st.empty()
+
+# # Exit loop if we will not receive data within timeoutsec.
+# timeoutsec = 30
+
+# # Simulate data from test_run() in placeholder.
+# while True:
+#     try:
+#         val, multiply = q.get(block=True, timeout=timeoutsec)
+#     except queue.Empty:
+#         break  # exit loop
+#     else:
+#         with placeholder.container():
+#             col1, col2 = st.columns(2)
+#             col1.metric(label="Current Value", value=val)
+#             col2.metric(label="Multiply by 10 ", value=multiply)
+#             q.task_done()
+
+
+
+
+
+
+
+
+
+
+# q = queue.Queue()
+
+# def test_run():
+#     for x in range(1, 100):
+#         time.sleep(1)
+#         val = x
+#         multiply = val * 10        
+#         q.put((val, multiply))
+
+# placeholder = st.empty()
+
+# timeoutsec = 30
+
+# # Simulate data from test_run() in placeholder.
+# while True:
+#     try:
+#         val, multiply = q.get(block=True, timeout=timeoutsec)
+#     except queue.Empty:
+#         break  # exit loop
+#     else:
+#         with placeholder.container():
+#             col1, col2 = st.columns(2)
+#             col1.metric(label="Current Value", value=val)
+#             col2.metric(label="Multiply by 10 ", value=multiply)
+#             q.task_done()
+
+
+
+
+
+
+
+st.write(f"網頁: { streamlit_js_eval(js_expressions='window.location.origin', want_output = True, key = 'LOC')}")
 
 loc = get_geolocation()
-# st.write(f"Your coordinates are {loc}")
 
-df=pd.DataFrame()
 col1, col2, col3 = st.columns(3)
-for i in range(100):
-    if loc is not None:
-        df_loc=pd.DataFrame(loc)
-        print(df_loc)
-        # st.write(f"緯度: {df_loc.loc['latitude', 'coords']}")
-        # st.write(f"經度: {df_loc.loc['longitude', 'coords']}")
-        # st.write(f"速度: {df_loc.loc['speed', 'coords']}")
-        
-        col1.metric(label="緯度", value=f"{df_loc.loc['latitude', 'coords']}")
-        col2.metric(label="經度", value=f"{df_loc.loc['longitude', 'coords']}")
-        col3.metric(label="速度", value=f"{df_loc.loc['speed', 'coords']}")
-        
-        dict_loc={"time":df_loc.loc["accuracy", "timestamp"],
-                  "緯度":df_loc.loc["latitude", "coords"],
-                  "經度":df_loc.loc["longitude", "coords"],
-                  "高度":df_loc.loc["altitude", "coords"],
-                  "heading":df_loc.loc["heading", "coords"],
-                  "速度":df_loc.loc["speed", "coords"]}
-        df=pd.concat([df, pd.DataFrame(dict_loc, index=[0])])
-    time.sleep(6)
+if st.button(label='開始量測'):
+    df=pd.DataFrame()
+    for i in range(100):
+        if loc is not None:    
+            print(loc)
+            print(type(loc)) # 使用dict來取得loc的內容
+            print()
+            print(loc['coords']['latitude'])
+            print(loc['coords']['longitude'])
+            print(loc['coords']['speed'])
+            print()
+            print()
 
-print(df)
-st.map(df, latitude='緯度', longitude='經度')
+            col1.metric(label="緯度", value=loc['coords']['latitude'])
+            col2.metric(label="經度", value=loc['coords']['longitude'])
+            col3.metric(label="速度", value=loc['coords']['speed'])
+            dict_loc={"timestamp":loc["timestamp"],
+                      "緯度":loc['coords']['latitude'],
+                      "經度":loc['coords']['longitude'],
+                      "高度":loc['coords']['altitude'],
+                      "heading":loc['coords']["heading"],
+                      "速度":loc['coords']['speed']}
+            df_i=pd.DataFrame(dict_loc, index=[i])
+            print(df_i)
+            print()
+            df=pd.concat([df, df_i])
+            time.sleep(6)
+    st.map(df, latitude='緯度', longitude='經度')
+    print(df)
+    print()
+    st.dataframe(df)
+
+
 
 
     
-# if True:
-#     x = streamlit_js_eval(js_expressions='window.innerWidth', key='WIDTH',  want_output = True,)                
-#     st.write(f"Width is {x}")
+
+
+    
+
+
+
+
 
 
 
